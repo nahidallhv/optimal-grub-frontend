e1c7c63b6b086ab92aff48309807ee9a537aa69d
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-10-11 05:20:10 +0400

    Updated

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
